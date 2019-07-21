36d26c1ff2b5463cf9bf4e181b1df0ec2f9ddb50
songseongbin <ppsosqq@gmail.com>
2019-07-22 00:30:36 +0900

    micro service aws 설정 변경

--- travis_scripts/deploy_to_docker_hub0.sh
+++ travis_scripts/deploy_to_docker_hub0.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push dockerever/tmx-authentication-service:$BUILD_NAME
-docker push dockerever/tmx-licensing-service:$BUILD_NAME
-docker push dockerever/tmx-organization-service:$BUILD_NAME
-docker push dockerever/tmx-confsvr:$BUILD_NAME
-docker push dockerever/tmx-eurekasvr:$BUILD_NAME
-docker push dockerever/tmx-zuulsvr:$BUILD_NAME
+docker push ppsosqq/tmx-authentication-service:$BUILD_NAME
+docker push ppsosqq/tmx-licensing-service:$BUILD_NAME
+docker push ppsosqq/tmx-organization-service:$BUILD_NAME
+docker push ppsosqq/tmx-confsvr:$BUILD_NAME
+docker push ppsosqq/tmx-eurekasvr:$BUILD_NAME
+docker push ppsosqq/tmx-zuulsvr:$BUILD_NAME
